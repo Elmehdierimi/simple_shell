495e3de1ec3225528c662e7e431c13a075eefea1
Elmehdi <elmehdierimi@gmail.com>
2024-08-29 07:15:37 -0700

    fsf

--- main.c
+++ main.c
@@ -28,8 +28,8 @@ int main(int argc, char *argv[], char *env[])
 }
 
 /**
- * handle_ctrl_c - Handles SIGINT (Ctrl+C) by printing a new prompt line
- * @opr: Optional parameter for the signal handler prototype
+ * handle_ctrl_c - print the prompt in a new line
+ * when the signal SIGINT (ctrl + c) is send to the program
  * @UNUSED: option of the prototype
  */
 void handle_ctrl_c(int opr UNUSED)
