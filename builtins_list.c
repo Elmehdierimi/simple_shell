fad902a03932e1e349ed0bc18c4da29bafb39616
Elmehdi <elmehdierimi@gmail.com>
2024-08-29 10:50:25 -0700

    hyeh

--- builtins_list.c
+++ builtins_list.c
@@ -1,15 +1,15 @@
 #include "shell.h"
 
 /**
- * builtins_list - search for match and execute the associate builtin
- * @data: struct for the program's data
- * Return: Returns the return of the function executed is there is a match,
+ * builtins_list - Finds and executes a matching built-in function
+ * @data: Pointer to the program's data structure
+ * Return: The return value of the executed function if a match is found,
  * otherwise returns -1.
- **/
+ */
 int builtins_list(data_of_program *data)
 {
-	int iterator;
-	builtins options[] = {
+	int index;
+	builtins builtin_options[] = {
 		{"exit", builtin_exit},
 		{"help", builtin_help},
 		{"cd", builtin_cd},
@@ -20,16 +20,17 @@ int builtins_list(data_of_program *data)
 		{NULL, NULL}
 	};
 
-/*walk through the structure*/
-	for (iterator = 0; options[iterator].builtin != NULL; iterator++)
+	/* Iterate through the built-ins array */
+	for (index = 0; builtin_options[index].builtin != NULL; index++)
 	{
-/*if there is a match between the given command and a builtin,*/
-		if (str_compare(options[iterator].builtin, data->command_name, 0))
+		/* Check if the command matches a built-in function */
+		if (str_compare(builtin_options[index].builtin, data->command_name, 0))
 		{
-/*execute the function, and return the return value of the function*/
-			return (options[iterator].function(data));
+			/* Execute the associated function and return its result */
+			return (builtin_options[index].function(data));
 		}
-/*if there is no match return -1 */
 	}
+
+	/* No match found, return -1 */
 	return (-1);
 }
