98741892c2aeabe4d547608fc9b30fb68e852fe5
Elmehdi <elmehdierimi@gmail.com>
2024-08-29 19:28:57 -0700

    de

--- str_token.c
+++ str_token.c
@@ -1,42 +1,42 @@
 #include "shell.h"
 /**
- * _strtok - it separates str con delimiters
- * @line: pointer to array
- * @delim: characters we mark off strings
- * Return: ptr to token
+ * _strtok - separates strings with delimiters
+ * @line: It´s pointer to array we receive in getline.
+ * @delim: It´s characters we mark off string in parts.
+ * Return: A pointer to the created token
 */
 char *_strtok(char *line, char *delim)
 {
+	int j;
 	static char *str;
-	char *cpstr;
-	int s;
+	char *copystr;
 
 	if (line != NULL)
 		str = line;
 	for (; *str != '\0'; str++)
 	{
-		for (s = 0; delim[s] != '\0'; s++)
+		for (j = 0; delim[j] != '\0'; j++)
 		{
-			if (*str == delim[s])
+			if (*str == delim[j])
 			break;
 		}
-		if (delim[s] == '\0')
+		if (delim[j] == '\0')
 			break;
 	}
-	cpstr = str;
-	if (*cpstr == '\0')
+	copystr = str;
+	if (*copystr == '\0')
 		return (NULL);
 	for (; *str != '\0'; str++)
 	{
-		for (s = 0; delim[s] != '\0'; s++)
+		for (j = 0; delim[j] != '\0'; j++)
 		{
-			if (*str == delim[s])
+			if (*str == delim[j])
 			{
 				*str = '\0';
 				str++;
-				return (cpstr);
+				return (copystr);
 			}
 		}
 	}
-	return (cpstr);
+	return (copystr);
 }
