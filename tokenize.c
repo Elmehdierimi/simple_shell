47ed4330133fbeaaa10acb2454714aa7ed76d844
Elmehdi <elmehdierimi@gmail.com>
2024-08-29 19:31:20 -0700

    sds

--- tokenize.c
+++ tokenize.c
@@ -1,43 +1,41 @@
 #include "shell.h"
 /**
- * tokenize - function separate string using delimiter
- * @data: pointer
- * Return: array of the string
+ * tokenize - this function separate the string using a designed delimiter
+ * @data: a pointer to the program's data
+ * Return: an array of the different parts of the string
  */
 void tokenize(data_of_program *data)
 {
-	char *delimit = " \t";
-	int s, j;
-	int count = 2;
-	int len;
+	char *delimiter = " \t";
+	int i, j, counter = 2, length;
 
-	len = strlen(data->input_line);
-	if (len)
+	length = str_length(data->input_line);
+	if (length)
 	{
-		if (data->input_line[len - 1] == '\n')
-			data->input_line[len - 1] = '\0';
+		if (data->input_line[length - 1] == '\n')
+			data->input_line[length - 1] = '\0';
 	}
 
-	for (s = 0; data->input_line[s]; s++)
+	for (i = 0; data->input_line[i]; i++)
 	{
-		for (r = 0; delimit[r]; r++)
+		for (j = 0; delimiter[j]; j++)
 		{
-			if (data->input_line[s] == delimit[r])
-				count++;
+			if (data->input_line[i] == delimiter[j])
+				counter++;
 		}
 	}
 
-	data->tokens = malloc(count * sizeof(char *));
+	data->tokens = malloc(counter * sizeof(char *));
 	if (data->tokens == NULL)
 	{
 		perror(data->program_name);
 		exit(errno);
 	}
-	s = 0;
-	data->tokens[s] = str_duplicate(_strtok(data->input_line, delimit));
+	i = 0;
+	data->tokens[i] = str_duplicate(_strtok(data->input_line, delimiter));
 	data->command_name = str_duplicate(data->tokens[0]);
-	while (data->tokens[s++])
+	while (data->tokens[i++])
 	{
-		data->tokens[s] = str_duplicate(_strtok(NULL, delimit));
+		data->tokens[i] = str_duplicate(_strtok(NULL, delimiter));
 	}
 }
