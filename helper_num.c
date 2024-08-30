89bae7c53bf9015874bc4ca928094583330b23c5
Elmehdi <elmehdierimi@gmail.com>
2024-08-29 20:00:56 -0700

    ss

--- helper_num.c
+++ helper_num.c
@@ -2,92 +2,85 @@
 
 /**
  * long_to_string - converts a number to a string.
- * @number: number to be converted to a string.
- * @string: buffer to save the number as a string.
- * @base: base to convert the number
+ * @number: number to be converten in an string.
+ * @string: buffer to save the number as string.
+ * @base: base to convert number
  *
  * Return: Nothing.
  */
 void long_to_string(long number, char *string, int base)
 {
-	int index = 0;
-	long quotient = number;
-	char digits[] = "0123456789abcdef";
-	int negative = 0;
+	int index = 0, inNegative = 0;
+	long cociente = number;
+	char letters[] = {"0123456789abcdef"};
 
-	if (number == 0)
-	{
+	if (cociente == 0)
 		string[index++] = '0';
-	}
 
-	if (number < 0)
-	{
-		negative = 1;
-		quotient = -number;
-	}
+	if (string[0] == '-')
+		inNegative = 1;
 
-	while (quotient != 0)
+	while (cociente)
 	{
-		string[index++] = digits[quotient % base];
-		quotient /= base;
+		if (cociente < 0)
+			string[index++] = letters[-(cociente % base)];
+		else
+			string[index++] = letters[cociente % base];
+		cociente /= base;
 	}
-
-	if (negative)
-	{
+	if (inNegative)
 		string[index++] = '-';
-	}
 
 	string[index] = '\0';
 	str_reverse(string);
 }
 
+
 /**
  * _atoi - convert a string to an integer.
  *
- * @s: pointer to the source string.
- * Return: integer representation of the string or 0.
+ * @s: pointer to str origen.
+ * Return: int of string or 0.
  */
 int _atoi(char *s)
 {
 	int sign = 1;
 	unsigned int number = 0;
-
-	while ((*s < '0' || *s > '9') && *s != '\0')
+	/*1- analisys sign*/
+	while (!('0' <= *s && *s <= '9') && *s != '\0')
 	{
 		if (*s == '-')
 			sign *= -1;
 		if (*s == '+')
-			sign *= 1;
+			sign *= +1;
 		s++;
 	}
 
-	while (*s >= '0' && *s <= '9' && *s != '\0')
+	/*2 - extract the number */
+	while ('0' <= *s && *s <= '9' && *s != '\0')
 	{
+
 		number = (number * 10) + (*s - '0');
 		s++;
 	}
-
 	return (number * sign);
 }
 
 /**
- * count_characters - count occurrences of a character in a string.
+ * count_characters - count the coincidences of character in string.
  *
- * @string: pointer to the source string.
- * @character: character to be counted.
- * Return: count of the character in the string.
+ * @string: pointer to str origen.
+ * @character: string with  chars to be counted
+ * Return: int of string or 0.
  */
 int count_characters(char *string, char *character)
 {
-	int i = 0;
-	int count = 0;
+	int i = 0, counter = 0;
 
-	while (string[i])
+	for (; string[i]; i++)
 	{
 		if (string[i] == character[0])
-			count++;
-		i++;
+			counter++;
 	}
-
-	return (count);
+	return (counter);
 }
